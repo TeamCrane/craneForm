a6979d742d87f2cedb07c40e3887f614e16c33a8
JYeolI <hn05057@naver.com>
2023-03-15 12:21:18 +0900

    (주열) 회원가입 완료 / 비밀번호 암호화 코드 작성

--- src/main/java/com/cafe24/craneform/Service/HomeService.java
+++ src/main/java/com/cafe24/craneform/Service/HomeService.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @Service
 public class HomeService {
-    public Map<String, Object> login (HttpSession session, @RequestParam String id, @RequestParam String pwd) {
+    public Map<String, Object> login (HttpSession session, @RequestParam String email, @RequestParam String pwd) {
         Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
         UserInfoDTO user = null;
 
@@ -28,8 +28,32 @@ public class HomeService {
         return resultMap;
     }
 
-    public Map<String, Object> join (@RequestBody UserInfoDTO user) {
+    public Map<String, Object> signUp (@RequestBody UserInfoDTO user) {
         Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+        if(user.getUi_name().length() > 20) {
+            resultMap.put("status", false);
+            resultMap.put("message", "이름은 20자를 넘을 수 없습니다.");
+            return resultMap;
+        }
+        else if(user.getUi_email().split("@").length == 2) {
+            resultMap.put("status", false);
+            resultMap.put("message", "이메일 형식을 확인해주세요.");
+            return resultMap;
+        }
+        else if(user.getUi_password().length() > 16) {
+            resultMap.put("status", false);
+            resultMap.put("message", "비밀번호는 16자를 넘을 수 없습니다.");
+            return resultMap;
+        }
+        else if(user.getUi_phone().length() != 10 && user.getUi_phone().length() != 11) {
+            resultMap.put("status", false);
+            resultMap.put("message", "전화번호 형식을 확인해주세요.\nex) 010XXXXOOOO");
+            return resultMap;
+        }
+
+//        member_mapper.insertMemberJoin(user);
+        resultMap.put("status", true);
+        resultMap.put("message", "가입되었습니다.");
 
         return resultMap;
     }
